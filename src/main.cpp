855969b3700e9bcda442bedf9046aa2cd94ad5ec
ViniGambini <vincentgoulet15@gmail.com>
2023-03-15 16:44:21 -0400

    receive from master +ACK

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,75 @@
 #include <mbed.h>
 
-#define ADDRESSE_I2C_PAR_DEFAUT 0x23
+#define ADDRESSE_I2C_PAR_DEFAUT 0x51 //1010001
 
 #if !DEVICE_I2CSLAVE
 #error [NOT_SUPPORTED] I2C Slave is not supported
 #endif
 
-static BufferedSerial serial_port(USBTX, USBRX);
+//static BufferedSerial serial_port(USBTX, USBRX);
 
 // Utiliser la classe I2CSlave pour créer un objet slave.
 // Consulter la documentation pour voir les méthodes disponibles.
-I2CSlave slave(I2C_SDA, I2C_SCL);
+I2CSlave slave(D14, D15);
 
 ///////////////////////////////////////////
 // Créer un objet debug_led à partir de la classe DigitalOut pour vous aider dans le debuggage
 ///////////////////////////////////////////
-DigitalOut led(LED2);
+//DigitalOut led(LED2);
 
 ///////////////////////////////////////////
 // Créer un objet moteur à partir de la classe PwmOut
 ///////////////////////////////////////////
-PwmOut moteur(D3);
+//PwmOut moteur(D3);
 
 
 ///////////////////////////////////////////
 // Créer une variable pour la machine à état qui gére le moteur (OFF ou Activé)
 ///////////////////////////////////////////
-enum{On, Off};
+//enum{On, Off};
 
-int etat = Off;
+//int etat = Off;
 
 int main() {
-  serial_port.set_baud(9600);
+  //serial_port.set_baud(9600);
 
-  char read_buffer[10];
-  char write_buffer[10];
+  char buffer[10];
+  
 
   slave.address(ADDRESSE_I2C_PAR_DEFAUT << 1);
 
-  double pourcentage = 0.075; //milieu
-  moteur.period(0.02);
+  //double pourcentage = 0.075; //milieu
+  //moteur.period(0.02);
 
   while (1) {
-        
+
+
         // Attendre une requête du master
-        int i = slave.receive();
+        int read = slave.receive();
 
         // Traiter la requête
-        switch (i) {
+        switch (read) {
 
             // Si le master envoie une requête de lecture
             case I2CSlave::ReadAddressed:
                 ///////////////////////////////////////////
                 // Retourner l'état du moteur (sa position ou OFF sous forme d'une chaine de caractères)
                 ///////////////////////////////////////////
-                //write_buffer[0] = etat;
 
-                slave.write(write_buffer, strlen(write_buffer) + 1); // Includes null char
+                slave.write(buffer, strlen(buffer) + 1); // Includes null char
                 break;
 
-            // Si le master envoie une requête de lecture qui nous est adressée
+            // Si le master envoie une requête d'écriture qui nous est adressée
             case I2CSlave::WriteAddressed:
-                slave.read(read_buffer, 10);
-                printf("Read A: %s\n\r", read_buffer);
+                slave.read(buffer, 10);
+                //printf("Read A: %s\n\r", buffer);
 
-                int8_t commande_recue = read_buffer[0];
+                
 
                 ///////////////////////////////////////////
                 // Modifier l'état du moteur en fonction de la commande reçue
                 ///////////////////////////////////////////
+                /*int8_t commande_recue = buffer[0];
                 switch (etat){
                     case Off:
                         moteur.suspend();
@@ -89,18 +90,15 @@ int main() {
                             etat = Off;
                         }
                         break;
-                }
+                }*/
                 break;
         }
         
         // Vider le buffer de lecture
-        for (int i = 0; i < sizeof(read_buffer); i++) {
-            read_buffer[i] = 0;
+        unsigned int i = 0;
+        for (i = 0; i < sizeof(buffer); i++) {
+            buffer[i] = 0;
         }
         
-        // Vider le buffer d'écriture
-        for (int i = 0; i < sizeof(write_buffer); i++) {
-            write_buffer[i] = 0;
-        }
     }
 }
